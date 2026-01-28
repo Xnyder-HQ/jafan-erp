ad59c4927a6c732ac36209a03d600dd34c46c72d
Nnaemekac <nnaemekac@users.noreply.github.com>
2026-01-28 19:44:18 +0100

    Add Netlify proxy to fix CORS issues

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = import.meta.env.DEV ? '/api' : 'https://jafanapi.xnyder.com';
+const API_BASE_URL = '/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
